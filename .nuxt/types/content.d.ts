0edd0732ac2498d1f263b31e36e07e0973c96254
Elsio Sanchez <elsiosanches@gmail.com>
2023-03-06 18:33:09 +0000

    Update 250 files

--- .nuxt/types/content.d.ts
+++ .nuxt/types/content.d.ts
@@ -1,4 +1,4 @@
 declare module '#content/server' {
-  const serverQueryContent: typeof import('/opt/Development/nuxt-component/atom/atom-Button/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
-  const parseContent: typeof import('/opt/Development/nuxt-component/atom/atom-Button/node_modules/@nuxt/content/dist/runtime/server').parseContent
+  const serverQueryContent: typeof import('/project/home/elsiosanchez/workspace/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
+  const parseContent: typeof import('/project/home/elsiosanchez/workspace/node_modules/@nuxt/content/dist/runtime/server').parseContent
 }
